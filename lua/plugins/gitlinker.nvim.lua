561fef10b79d6c035958dee65c215819997fdee5
André Cruz <acruz@cloudflare.com>
2024-10-11 17:43:59 +0100

    Added gitlinker

---
+++ lua/plugins/gitlinker.nvim.lua
@@ -0,0 +1,36 @@
+return {
+  "linrongbin16/gitlinker.nvim",
+  opts = function(_, _)
+    return {
+      router = {
+        browse = {
+          ["^git.cfdata.org$"] = "https://bitbucket.cfdata.org/projects/"
+            .. "{_A.ORG}/repos/"
+            .. "{_A.REPO}/browse/"
+            .. "{_A.FILE}"
+            .. "?at={_A.REV}"
+            .. "#{_A.LSTART}"
+            .. "{(_A.LEND > _A.LSTART and ('-' .. _A.LEND) or '')}",
+        },
+      },
+    }
+  end,
+  keys = {
+    {
+      "<leader>gy",
+      "<cmd>GitLink<cr>",
+      silent = true,
+      noremap = true,
+      desc = "Copy git permlink to clipboard",
+      mode = { "n", "v" },
+    },
+    {
+      "<leader>gY",
+      "<cmd>GitLink!<cr>",
+      silent = true,
+      noremap = true,
+      desc = "Open git permlink in browser",
+      mode = { "n", "v" },
+    },
+  },
+}
