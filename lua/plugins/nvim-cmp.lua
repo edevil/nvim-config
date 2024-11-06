552bf5d5592ed7c3abd288f9f0c17accabc8837e
André Cruz <acruz@cloudflare.com>
2024-11-06 15:59:53 +0000

    Remove snippets from auto complete suggestions

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -8,14 +8,19 @@ return {
       --   return not context.in_treesitter_capture("comment")
       -- end
       opts.sources = vim.tbl_filter(function(source)
-        return not vim.tbl_contains({ "buffer", "nvim_lsp" }, source.name)
+        return not vim.tbl_contains({ "buffer", "nvim_lsp", "luasnip", "snippy" }, source.name)
       end, opts.sources)
       table.insert(opts.sources, 1, {
         name = "nvim_lsp",
         entry_filter = function(entry, _)
-          return require("cmp.types").lsp.CompletionItemKind[entry:get_kind()] ~= "Text"
+          completion_kind = require("cmp.types").lsp.CompletionItemKind[entry:get_kind()]
+          return not vim.tbl_contains({ "Text", "Snippet" }, completion_kind)
         end,
       })
     end,
   },
+
+  -- Disable other snippet-related plugins
+  { "L3MON4D3/LuaSnip", enabled = false },
+  { "rafamadriz/friendly-snippets", enabled = false },
 }
