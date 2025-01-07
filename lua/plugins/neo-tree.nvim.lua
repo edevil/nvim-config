2058b92e401bed7fc2688bb3deef42a299c5962a
André Cruz <acruz@cloudflare.com>
2025-01-07 10:48:35 +0000

    Moved from telecope to fzf-lua

--- lua/plugins/neo-tree.nvim.lua
+++ lua/plugins/neo-tree.nvim.lua
@@ -1,45 +1,22 @@
-local function getTelescopeOpts(state, path)
-  return {
-    cwd = path,
-    search_dirs = { path },
-    attach_mappings = function(prompt_bufnr, map)
-      local actions = require("telescope.actions")
-      actions.select_default:replace(function()
-        actions.close(prompt_bufnr)
-        local action_state = require("telescope.actions.state")
-        local selection = action_state.get_selected_entry()
-        local filename = selection.filename
-        if filename == nil then
-          filename = selection[1]
-        end
-        -- any way to open the file without triggering auto-close event of neo-tree?
-        require("neo-tree.sources.filesystem").navigate(state, state.path, filename)
-      end)
-      return true
-    end,
-  }
-end
-
 return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = {
     window = {
       mappings = {
-        ["t"] = "none",
-        ["tf"] = "telescope_find",
-        ["tg"] = "telescope_grep",
+        ["F"] = "find_files",
+        ["G"] = "live_grep",
       },
     },
     commands = {
-      telescope_find = function(state)
+      find_files = function(state)
         local node = state.tree:get_node()
         local path = node:get_id()
-        require("telescope.builtin").find_files(getTelescopeOpts(state, path))
+        require("fzf-lua").files({ cwd = path })
       end,
-      telescope_grep = function(state)
+      live_grep = function(state)
         local node = state.tree:get_node()
         local path = node:get_id()
-        require("telescope.builtin").live_grep(getTelescopeOpts(state, path))
+        require("fzf-lua").live_grep({ cwd = path })
       end,
     },
   },
